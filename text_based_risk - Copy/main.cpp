e4989fbd40993182494c2a40393b302d72199897
Jillian McBride <jillian.mcbride@wsu.edu>
2025-04-25 19:21:10 -0700

    Update main.cpp

--- text_based_risk - Copy/main.cpp
+++ text_based_risk - Copy/main.cpp
@@ -14,11 +14,48 @@ int main(void)
 {
 	std::srand(std::time(0));
 
-	GamePlay cheese; 
+	int optionSelector = 0;
+
+do
+{
+	system("cls");
+
+	std::cout << "Select what you want to do here\n\n\t>1. test \n\n\t>2. Play\n\n\t\t>";
+
+	std::cin >> optionSelector;
+
+}while (!(optionSelector == 1 || optionSelector == 2));
+
+system("cls");
+
+switch (optionSelector)
+{
 	
-	cheese.risk();  
+case 2:
+{
+	system("pause");
+
+	GamePlay cheese;
+
+	cheese.risk();
 
 	//TerritoryArray test(18);
+
+	break;
+}
+case 1:
+{
+	adjacencyMatrixTest matrixTest;
+
+	matrixTest.runTest();
+
+	break;
+}
+default:
+	break;
+}
+
+
 	
 	
 
